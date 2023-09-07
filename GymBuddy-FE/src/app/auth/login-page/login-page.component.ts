dd8f8efbe280eba6a92fa15873d2d0c4509b8ddd
Nikola Grbovic <grbovicn.ra@gmail.com>
2023-09-07 19:41:48 +0200

    feat/finish login FE

--- GymBuddy-FE/src/app/auth/login-page/login-page.component.ts
+++ GymBuddy-FE/src/app/auth/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder, private service: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.minLength(6)]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      }
+    )
+  }
+  onSubmit = () => {
+    console.log("submit");
+    if (this.loginForm.valid) {
+      let dto = {
+        email: this.loginForm.get('email')?.value,
+        password: this.loginForm.get('password')?.value
+      }
+      console.log(dto);
+    }
   }
 
 }
